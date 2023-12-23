c84e4a1e8e7fab25f9fcdbfb88b1a9725bb8a3fd
Malte Delfs <delfsmalte@gmail.com>
2023-12-23 11:44:50 +0000

    WIP

--- unwall.js
+++ unwall.js
@@ -1,15 +1,16 @@
 const mainContainer = document.querySelector("[class*=paywalledContent]");
-var p = document.querySelector("[class*=paywalledContent] > p");
 
-function renderItems(items, container, nodeType = "p") {
+function renderItems(items, container, afterNode, nodeType = "p") {
     console.log("renderItems", items, container, nodeType);
     items.forEach(e => {
         if (e.type == "ad" || e.type == "piano" || e.type == "newsletterAd"){
             return;
         }
         var txt = e.text;
+        var skipInnerHtml = false;
         if (e.type == "list"){
-            nodeType = "ul" ;  
+            nodeType = "ul" ; 
+            skipInnerHtml = true;
         }
         const node = document.createElement(nodeType);
         node.className = p.className;
@@ -34,20 +35,24 @@ function renderItems(items, container, nodeType = "p") {
             txt = e.html;
         }
         container.appendChild(node);
-        node.innerHTML = txt || ("TODO: implement type " + e.type);
+        if (!skipInnerHtml){
+            node.innerHTML = txt || ("TODO: implement type " + e.type);
+        }
         if (img){
             node.appendChild(img);
         }
-        p.after(node);
+        if (afterNode){
+            afterNode.after(node);
+        }
         if (e.type == "list"){
             var listItems = e.list.items.map(i => ({...i, type: "text"}))
-            renderItems(listItems, node, "li");
+            renderItems(listItems, node, null, "li");
         }
-        p = node;
+        afterNode = node;
     });
 }
 
-renderItems(Fusion.globalContent.elements, mainContainer);
+renderItems(Fusion.globalContent.elements, mainContainer, document.querySelector("[class*=paywalledContent] > p"));
 document.querySelector("[class*=paywalledContent]").style = "height:fit-content!important";
 document.querySelector("#piano-lightbox-article-kn").remove();
 document.querySelector("[class*=RecommendationContainer]").remove();
